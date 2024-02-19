378228b61c495affb379a3ba14a318142e31a351
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-02-19 09:37:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Rudy-ChunChunmaru/",
+  base: "/My_Main_Web/",
 });
