a574e73227009d214693c0fabf17961cc03776f0
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-05-09 16:33:17 +0700

    2024-05-09 (contact me update)

--- src/scenes/contactme/contactform.tsx
+++ src/scenes/contactme/contactform.tsx
@@ -7,14 +7,11 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
-  const [name, SetName] = useState("");
-  const [email, SetEmail] = useState("");
-  const [message, SetMessage] = useState("");
-
   const {
     register,
     trigger,
     getValues,
+    setValue,
     formState: { errors },
   } = useForm();
 
@@ -54,29 +51,30 @@ const ContactForm = () => {
       const SERVICE_ID = "service_h00pj9i";
       const TEMPLATE_ID = "template_qqopk77";
       const TEMPLATE_PARAMS = {
-        from_title: "MY_MAIN_WEB - Message - " + email,
-        from_name: name,
-        from_email: email,
-        from_message: message,
+        from_title: "MY_MAIN_WEB - Message - " + getValues("from_email"),
+        from_name: getValues("from_name"),
+        from_email: getValues("from_email"),
+        from_message: getValues("from_message"),
       };
       const USER_ID = "Je0IIZkyQ11JSYHRx";
 
       emailjs.send(SERVICE_ID, TEMPLATE_ID, TEMPLATE_PARAMS, USER_ID).then(
         (result) => {
           console.log("berhasil terkirim! <br>", result);
-          SetName("");
-          SetEmail("");
-          SetMessage("");
+          setValue("from_name", "");
+          setValue("from_email", "");
+          setValue("from_message", "");
         },
         (error) => {
           console.log("gagal terkirim! <br>", error.text);
+          alert("gagal terkirim! <br>" + error.text);
         }
       );
     }
   };
 
   return (
-    <div className=" mx-auto mb-2 w-10/12 rounded-md bg-slate-300 px-5 py-7">
+    <div className=" mx-auto mb-2 w-full rounded-md bg-slate-300 px-5 py-7 sm:w-10/12">
       <form
         target="_blank"
         method="POST"
@@ -122,16 +120,16 @@ const ContactForm = () => {
         <textarea
           className="relative z-0 h-24 w-full rounded-lg px-2 py-1"
           placeholder="Message"
-          {...register("form_message", {
+          {...register("from_message", {
             required: true,
             maxLength: 1000,
           })}
         ></textarea>
-        {errors.form_message &&
-          errors.form_message.type === "required" &&
+        {errors.from_message &&
+          errors.from_message.type === "required" &&
           errormessageview("This field is required.")}
-        {errors.form_message &&
-          errors.form_message.type === "maxLength" &&
+        {errors.from_message &&
+          errors.from_message.type === "maxLength" &&
           errormessageview("Max Length is 1000 char.")}
 
         <div className="flex w-full justify-center">
@@ -139,11 +137,6 @@ const ContactForm = () => {
             <input
               type="submit"
               value="Form Submit"
-              onClick={() => {
-                () => SetName(getValues("from_name"));
-                () => SetEmail(getValues("from_email"));
-                () => SetMessage(getValues("form_message"));
-              }}
               className="relative z-0 rounded-lg border-2 border-slate-900 p-2 hover:border-slate-600 hover:bg-slate-400"
             />
           </strong>
