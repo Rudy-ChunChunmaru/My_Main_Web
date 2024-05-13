a6c043818625ff49d8d9d6ee0e7dcb146949b9a7
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-05-13 08:44:26 +0700

    2024-05-13

--- src/scenes/profile/Experience_detail_expan_no.tsx
+++ src/scenes/profile/Experience_detail_expan_no.tsx
@@ -17,17 +17,10 @@ const Experience_detail_expan_no = ({ detail }: Props) => {
     else return "bg-slate-600";
   };
   let numword = 0;
-  let numcounter = 0;
   const [datadetail, setdatadetail] = useState(detail[numword]);
-  const [counter, setcounter] = useState(0);
+
   useEffect(() => {
     let longcounter = 4000;
-
-    const intervalcounter = setInterval(() => {
-      numcounter++;
-      if (numcounter >= longcounter / 1000) numcounter = 0;
-      setcounter(numcounter);
-    }, 1000);
 
     const interval = setInterval(() => {
       numword++;
@@ -36,60 +29,36 @@ const Experience_detail_expan_no = ({ detail }: Props) => {
     }, longcounter);
     // Clean up the interval on component unmount
     return () => {
-      clearInterval(intervalcounter);
       clearInterval(interval);
     };
   }, []);
 
   return (
-    <motion.div
-      className="flex w-full rounded-md bg-slate-300 p-1 text-black"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      transition={{ duration: 0.5 }}
-      variants={{
-        hidden: { opacity: 0.25, y: -20 },
-        visible: { opacity: 1, y: 0 },
-      }}
-    >
-      {/* <span className="text-center font-Protest_Riot text-3xl">
-        {datadetail.info}
-      </span> */}
-      <div
-        className={
-          "hidden rounded-md px-1 text-white sm:block " +
-          classnamediv(datadetail.doing)
-        }
+    <div>
+      <motion.div
+        className="flex w-full rounded-md bg-slate-300 p-1 text-black"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0.25, y: -20 },
+          visible: { opacity: 1, y: 0 },
+        }}
       >
-        {datadetail.doing}
-      </div>
-      <div className="flex w-full justify-between">
-        <div className="pl-2">{datadetail.info}</div>
-        <div className="pl-2">
-          {counter == 0 && (
-            <div className="h-6 w-6 animate-spin rounded-full border-b-4 border-black">
-              I
-            </div>
-          )}
-          {counter == 1 && (
-            <div className="h-6 w-6 animate-spin rounded-full border-b-4 border-l-4 border-black">
-              II
-            </div>
-          )}
-          {counter == 2 && (
-            <div className="h-6 w-6 animate-spin rounded-full border-b-4 border-l-4 border-t-4 border-black">
-              III
-            </div>
-          )}
-          {counter == 3 && (
-            <div className="h-6 w-6 animate-spin rounded-full border-4 border-black">
-              IIII
-            </div>
-          )}
+        <div
+          className={
+            "hidden w-[15%] rounded-md px-1 text-center text-white sm:block " +
+            classnamediv(datadetail.doing)
+          }
+        >
+          {datadetail.time} - {datadetail.doing}
         </div>
-      </div>
-    </motion.div>
+        <div className="flex w-full justify-between">
+          <div className="pl-2">{datadetail.info}</div>
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
