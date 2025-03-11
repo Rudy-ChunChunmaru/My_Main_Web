a473bc2d1a05d0585a960974cf337caf70fdf0a3
Rudy <rudymu1998@gmail.com>
2025-03-11 02:35:14 +0000

    probelm data route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import { Index } from "@/scenes/index";
 
 function App() {
   return (
     <div className="w-[100%] h-[100%]">
-      <Routes>
-        <Route key="0" path="my_main_web" element={<Index />} />
-      </Routes>
+      <Index />
     </div>
   ) 
 }
