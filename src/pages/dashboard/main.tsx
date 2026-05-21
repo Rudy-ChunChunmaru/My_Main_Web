84a2e411d7337e23919e48c1e4be313c3580a3b8
Rudy <95736349+rudymu1998@users.noreply.github.com>
2026-05-21 19:17:00 +0700

    fixing navbar for phone

--- src/pages/dashboard/main.tsx
+++ src/pages/dashboard/main.tsx
@@ -38,7 +38,7 @@ export function Main({children}:MainProps){
                 initial="visible"
                 animate={controls}
                 exit="hidden"
-                className="fixed flex w-full bg-[#ffffff4a] px-2 py-1 gap-3 drop-shadow-xl justify-end sm:justify-start"
+                className="fixed flex w-full bg-[#ffffff4a] px-2 py-1 my-9 gap-3 drop-shadow-xl justify-start sm:justify-start sm:my-0"
             >
                 <div className="border-2 rounded-lg border-black bg-gray-500">
                     <SidebarTrigger />
